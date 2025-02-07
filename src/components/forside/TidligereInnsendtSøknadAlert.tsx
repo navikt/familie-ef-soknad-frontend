09bccb3d3544d785114fdf8e726b009c16a24455
krikof-nav <kristian.kofoed@nav.no>
2025-02-07 09:52:54 +0100

    La til en feature toggle alert som sjekker om bruker allerede har noen tidligere innsendte søknader basert på stønad. Alerten vises hvis feature toggle er skrudd på, hvis man finner noen tidligere søknader og stønadstypen passer inngangen til stønaden man søker på. Vi viser nå en generell blå alert med noen generelle tekster, men disse skal endres på siden.

---
+++ src/components/forside/TidligereInnsendtSøknadAlert.tsx
@@ -0,0 +1,67 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Alert, Heading } from '@navikt/ds-react';
+import { Stønadstype } from '../../models/søknad/stønadstyper';
+import Environment from '../../Environment';
+
+export interface SistInnsendtSøknad {
+  søknadsdato: string;
+  stønadType: Stønadstype;
+}
+
+interface TidligereInnsendtSøknadAlertProps {
+  stønadType: Stønadstype;
+}
+
+export const TidligereInnsendtSøknadAlert: React.FC<
+  TidligereInnsendtSøknadAlertProps
+> = ({ stønadType }) => {
+  const [aktiveSøknader, settAktiveSøknader] = useState<SistInnsendtSøknad[]>(
+    []
+  );
+
+  const hentAktiveSøknader = useCallback(() => {
+    axios
+      .get<SistInnsendtSøknad[]>(
+        Environment().apiProxyUrl +
+          '/api/soknadskvittering/sist-innsendt-per-stonad'
+      )
+      .then((response) => {
+        settAktiveSøknader(response.data);
+      })
+      .catch((error) => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    hentAktiveSøknader();
+  }, [hentAktiveSøknader]);
+
+  const visNylingInnsendtSøknadAlert = aktiveSøknader.some(
+    (søknad) => søknad.stønadType.valueOf().toLowerCase() === stønadType
+  );
+
+  return (
+    <>
+      {visNylingInnsendtSøknadAlert && (
+        <Alert variant="info">
+          <Heading spacing size="small" level="3">
+            Du har allerede en aktiv søknad hos oss
+          </Heading>
+          <p>
+            Vi ser at du nylig har sendt inn denne søknaden. Dersom du sender
+            søknaden på nytt, vil behandlingen ta lenger tid. Ønsker du å
+            opplyse om endringer eller noe nytt kan du gjøre følgende:
+          </p>
+          <ul>
+            <li>endre kontonummeret</li>
+            <li>melde fra om frivillig skattetrekk på barnepensjonen</li>
+            <li>ettersende dokumentasjon</li>
+            <li>Er det noe annet du ønsker å melde inn kan du kontakte oss</li>
+          </ul>
+        </Alert>
+      )}
+    </>
+  );
+};
