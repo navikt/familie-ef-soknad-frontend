a1f7b7f524364990def1613375bd0adbbb38b193
krikof-nav <kristian.kofoed@nav.no>
2025-02-13 12:29:27 +0100

    Refaktorering av kode.

--- src/components/forside/TidligereInnsendteSøknaderAlert.tsx
+++ src/components/forside/TidligereInnsendteSøknaderAlert.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import { Alert, Heading } from '@navikt/ds-react';
 import { Stønadstype } from '../../models/søknad/stønadstyper';
@@ -16,17 +16,6 @@ interface TidligereInnsendteSøknadAlertProps {
   stønadType: Stønadstype;
 }
 
-const ettersendUrls = {
-  [Stønadstype.overgangsstønad]:
-    'https://www.nav.no/start/ettersend-soknad-overgangsstonad-enslig',
-  [Stønadstype.barnetilsyn]:
-    'https://www.nav.no/start/ettersend-soknad-barnetilsyn-enslig',
-  [Stønadstype.skolepenger]:
-    'https://www.nav.no/start/ettersend-soknad-skolepenger-enslig',
-};
-
-const kontaktOssUrl = 'https://www.nav.no/kontakt-oss';
-
 export const TidligereInnsendteSøknaderAlert: React.FC<
   TidligereInnsendteSøknadAlertProps
 > = ({ stønadType }) => {
@@ -38,11 +27,24 @@ export const TidligereInnsendteSøknaderAlert: React.FC<
     SistInnsendteSøknad[]
   >([]);
 
+  const ettersendUrls = useMemo(
+    () => ({
+      [Stønadstype.overgangsstønad]:
+        'https://www.nav.no/start/ettersend-soknad-overgangsstonad-enslig',
+      [Stønadstype.barnetilsyn]:
+        'https://www.nav.no/start/ettersend-soknad-barnetilsyn-enslig',
+      [Stønadstype.skolepenger]:
+        'https://www.nav.no/start/ettersend-soknad-skolepenger-enslig',
+    }),
+    []
+  );
+
+  const kontaktOssUrl = 'https://www.nav.no/kontakt-oss';
+
   const hentInnsendteSøknader = useCallback(() => {
     axios
       .get<SistInnsendteSøknad[]>(
-        Environment().apiProxyUrl +
-          '/api/soknadskvittering/sist-innsendt-per-stonad'
+        `${Environment().apiProxyUrl}/api/soknadskvittering/sist-innsendt-per-stonad`
       )
       .then((response) => {
         settInnsendteSøknader(response.data);
@@ -61,46 +63,40 @@ export const TidligereInnsendteSøknaderAlert: React.FC<
     }
   }, [hentInnsendteSøknader, hentSistInnsendteSøknadPerStønad]);
 
-  const visNylingInnsendtSøknadAlert = innsendteSøknader.some(
-    (søknad) => søknad.stønadType.valueOf().toLowerCase() === stønadType
-  );
-
   const gjeldeneSøknad = innsendteSøknader.find(
     (søknad) => søknad.stønadType.valueOf().toLowerCase() === stønadType
   );
 
-  if (gjeldeneSøknad != null) {
-    return (
-      <>
-        {visNylingInnsendtSøknadAlert && (
-          <Alert variant="info">
-            <Heading spacing size="small" level="3">
-              Du har nylig sendt inn en søknad til oss
-            </Heading>
-            <p>
-              {`Du søkte om ${stønadType} den ${formatDate(gjeldeneSøknad.søknadsdato)}.`}
-            </p>
-            <p>
-              Hvis du ikke fikk lastet opp all dokumentasjon da du søkte, kan du{' '}
-              <a
-                href={ettersendUrls[stønadType]}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                ettersende det som mangler
-              </a>
-              .
-            </p>
-            <p>
-              Du kan også si ifra om endringer ved å{' '}
-              <a href={kontaktOssUrl} target="_blank" rel="noopener noreferrer">
-                skrive en beskjed til oss
-              </a>
-              .
-            </p>
-          </Alert>
-        )}
-      </>
-    );
+  if (!gjeldeneSøknad) {
+    return null;
   }
+
+  return (
+    <Alert variant="info">
+      <Heading spacing size="small" level="3">
+        Du har nylig sendt inn en søknad til oss
+      </Heading>
+      <p>
+        {`Du søkte om ${stønadType} den ${formatDate(gjeldeneSøknad.søknadsdato)}.`}
+      </p>
+      <p>
+        Hvis du ikke fikk lastet opp all dokumentasjon da du søkte, kan du{' '}
+        <a
+          href={ettersendUrls[stønadType]}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ettersende det som mangler
+        </a>
+        .
+      </p>
+      <p>
+        Du kan også si ifra om endringer ved å{' '}
+        <a href={kontaktOssUrl} target="_blank" rel="noopener noreferrer">
+          skrive en beskjed til oss
+        </a>
+        .
+      </p>
+    </Alert>
+  );
 };
