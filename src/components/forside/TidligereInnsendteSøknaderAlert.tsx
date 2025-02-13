44ae401a74910a1dfb492df76f4193f638f04040
krikof-nav <kristian.kofoed@nav.no>
2025-02-13 15:04:52 +0100

    Tekster for tidligere innsendt søknad varsel er nå støttet på engelsk.

--- src/components/forside/TidligereInnsendteSøknaderAlert.tsx
+++ src/components/forside/TidligereInnsendteSøknaderAlert.tsx
@@ -6,6 +6,7 @@ import Environment from '../../Environment';
 import { useToggles } from '../../context/TogglesContext';
 import { ToggleName } from '../../models/søknad/toggles';
 import { formatDate, strengTilDato } from '../../utils/dato';
+import { useSpråkContext } from '../../context/SpråkContext';
 
 export interface SistInnsendteSøknad {
   søknadsdato: string;
@@ -16,27 +17,28 @@ interface TidligereInnsendteSøknadAlertProps {
   stønadType: Stønadstype;
 }
 
+const ettersendUrls = {
+  [Stønadstype.overgangsstønad]:
+    'https://www.nav.no/start/ettersend-soknad-overgangsstonad-enslig',
+  [Stønadstype.barnetilsyn]:
+    'https://www.nav.no/start/ettersend-soknad-barnetilsyn-enslig',
+  [Stønadstype.skolepenger]:
+    'https://www.nav.no/start/ettersend-soknad-skolepenger-enslig',
+};
+
+const kontaktOssUrl = 'https://www.nav.no/kontakt-oss';
+
 export const TidligereInnsendteSøknaderAlert: React.FC<
   TidligereInnsendteSøknadAlertProps
 > = ({ stønadType }) => {
   const { toggles } = useToggles();
   const hentSistInnsendteSøknadPerStønad =
     toggles[ToggleName.hentSistInnsendteSøknadPerStønad];
+  const [locale] = useSpråkContext();
 
   const [innsendteSøknader, settInnsendteSøknader] = useState<
     SistInnsendteSøknad[]
   >([]);
-
-  const ettersendingUrler = {
-    [Stønadstype.overgangsstønad]:
-      'https://www.nav.no/start/ettersend-soknad-overgangsstonad-enslig',
-    [Stønadstype.barnetilsyn]:
-      'https://www.nav.no/start/ettersend-soknad-barnetilsyn-enslig',
-    [Stønadstype.skolepenger]:
-      'https://www.nav.no/start/ettersend-soknad-skolepenger-enslig',
-  };
-
-  const kontaktOssUrl = 'https://www.nav.no/kontakt-oss';
 
   const hentInnsendteSøknader = useCallback(() => {
     axios
@@ -68,30 +70,60 @@ export const TidligereInnsendteSøknaderAlert: React.FC<
     return null;
   }
 
+  const tekster = {
+    nb: {
+      heading: 'Du har nylig sendt inn en søknad til oss',
+      søkteOm: `Du søkte om ${stønadType} den ${formatDate(strengTilDato(gjeldeneSøknad.søknadsdato))}.`,
+      ettersende:
+        'Hvis du ikke fikk lastet opp all dokumentasjon da du søkte, kan du',
+      ettersendeLink: 'ettersende det som mangler',
+      endringer: 'Du kan også si ifra om endringer ved å',
+      endringerLink: 'skrive en beskjed til oss',
+    },
+    nn: {
+      heading: 'Du har nyleg sendt inn ein søknad til oss',
+      søkteOm: `Du søkte om ${stønadType} den ${formatDate(strengTilDato(gjeldeneSøknad.søknadsdato))}.`,
+      ettersende:
+        'Viss du ikkje fekk lasta opp all dokumentasjon då du søkte, kan du',
+      ettersendeLink: 'ettersenda det som manglar',
+      endringer: 'Du kan òg seie ifrå om endringar ved å',
+      endringerLink: 'skrive ei melding til oss',
+    },
+    en: {
+      heading: 'You have recently submitted an application to us',
+      søkteOm: `You applied for ${stønadType} on ${formatDate(strengTilDato(gjeldeneSøknad.søknadsdato))}.`,
+      ettersende:
+        'If you did not upload all the documentation when you applied, you can',
+      ettersendeLink: 'submit the missing documents',
+      endringer: 'You can also notify us of changes by',
+      endringerLink: 'writing a message to us',
+    },
+  };
+
+  const currentTexts = tekster[locale];
+
   return (
     <Alert variant="info">
       <Heading spacing size="small" level="3">
-        Du har nylig sendt inn en søknad til oss
+        {currentTexts.heading}
       </Heading>
-      <p>
-        {`Du søkte om ${stønadType} den ${formatDate(strengTilDato(gjeldeneSøknad.søknadsdato))}.`}
-      </p>
+      <p>{currentTexts.søkteOm}</p>
       <ul>
         <li>
-          Hvis du ikke fikk lastet opp all dokumentasjon da du søkte, kan du{' '}
+          {currentTexts.ettersende}{' '}
           <a
-            href={ettersendingUrler[stønadType]}
+            href={ettersendUrls[stønadType]}
             target="_blank"
             rel="noopener noreferrer"
           >
-            ettersende det som mangler
+            {currentTexts.ettersendeLink}
           </a>
           .
         </li>
         <li>
-          Du kan også si ifra om endringer ved å{' '}
+          {currentTexts.endringer}{' '}
           <a href={kontaktOssUrl} target="_blank" rel="noopener noreferrer">
-            skrive en beskjed til oss
+            {currentTexts.endringerLink}
           </a>
           .
         </li>
