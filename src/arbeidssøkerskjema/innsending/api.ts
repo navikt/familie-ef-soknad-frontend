1d53a8beed987a993795330789d4cd1faa677b77
Viktor Solberg <viktorgsolberg@gmail.com>
2025-03-24 14:04:39 +0100

    skal fjerne feature-toggle for gammel vs. ny innsendingsflyt

--- src/arbeidssøkerskjema/innsending/api.ts
+++ src/arbeidssøkerskjema/innsending/api.ts
@@ -1,18 +1,6 @@
 import axios from 'axios';
 import Environment from '../../Environment';
 
-export const sendInnSkjema = async (skjema: object) => {
-  const response = await axios.post(
-    `${Environment().apiProxyUrl}/api/registrerarbeid`,
-    skjema,
-    {
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      withCredentials: true,
-    }
-  );
-  return response.data;
-};
-
 export const sendInnArbeidssøkerSkjema = async (skjema: object) => {
   const response = await axios.post(
     `${Environment().apiProxyUrl}/api/soknadskvittering/arbeidssoker`,
