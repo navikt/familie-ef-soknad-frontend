a877a4e3dab17d337aa70f2ac6d6d345aa02e999
krikof-nav <kristian.kofoed@nav.no>
2025-06-06 12:39:25 +0200

    Liten refaktorering av Checkbox.

--- src/components/spørsmål/komponent/nyrefaktorering/kompleksespørsmål/OmDenTidligereSamboerenDin.tsx
+++ src/components/spørsmål/komponent/nyrefaktorering/kompleksespørsmål/OmDenTidligereSamboerenDin.tsx
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { useLokalIntlContext } from '../../../../../context/LokalIntlContext';
 import {
   Checkbox,
-  CheckboxGroup,
   Heading,
   TextField,
   VStack,
   DatePicker,
   useDatepicker,
 } from '@navikt/ds-react';
+import { useLokalIntlContext } from '../../../../../context/LokalIntlContext';
 import { hentTekst } from '../../../../../utils/søknad';
 import { identErGyldig } from '../../../../../utils/validering/validering';
 
 export const OmDenTidligereSamboerenDin: React.FC = () => {
   const intl = useLokalIntlContext();
 
+  const [navn, settNavn] = useState('');
   const [ident, settIdent] = useState('');
   const [visFeil, settVisFeil] = useState(false);
+  const [brukerIkkeIdent, settBrukerIkkeIdent] = useState(false);
 
-  const handleCheckboxChange = (val: string[]) => {
-    console.info('Checkbox selected:', val);
-  };
+  const [fødselsdatoVerdi, settFødselsdatoVerdi] = useState<Date | undefined>();
+  const [flyttedatoVerdi, settFlyttedatoVerdi] = useState<Date | undefined>();
 
+  // TODO: Husk å sende med flytteDatoVerdi:
+  flyttedatoVerdi;
+
+  // TODO: Husk å gjøre det mulig å velge år.
   const fødselsdato = useDatepicker({
-    onDateChange: (date) => console.info('Fødselsdato valgt:', date),
+    onDateChange: settFødselsdatoVerdi,
   });
 
   const flyttetFraDato = useDatepicker({
     toDate: new Date(),
-    onDateChange: (date) =>
-      console.info('Flyttet fra hverandre dato valgt:', date),
+    onDateChange: settFlyttedatoVerdi,
   });
 
   // Liten debounce som stopper feilmelding fra å dukke opp sekundet bruker skriver.
   useEffect(() => {
     const timeout = setTimeout(() => {
-      if (ident.trim() !== '') {
+      if (ident.trim() !== '' && !brukerIkkeIdent) {
         settVisFeil(!identErGyldig(ident));
       } else {
         settVisFeil(false);
@@ -43,7 +46,12 @@ export const OmDenTidligereSamboerenDin: React.FC = () => {
     }, 250);
 
     return () => clearTimeout(timeout);
-  }, [ident]);
+  }, [ident, brukerIkkeIdent]);
+
+  const skalViseFødselsdato = brukerIkkeIdent;
+  const skalViseFlyttedato =
+    (brukerIkkeIdent && navn.trim() !== '' && fødselsdatoVerdi) ||
+    (!brukerIkkeIdent && navn.trim() !== '' && identErGyldig(ident));
 
   return (
     <VStack gap="6" align="start">
@@ -51,40 +59,58 @@ export const OmDenTidligereSamboerenDin: React.FC = () => {
         {hentTekst('sivilstatus.tittel.samlivsbruddAndre', intl)}
       </Heading>
 
-      <TextField label={hentTekst('person.navn', intl)} />
+      <TextField
+        label={hentTekst('person.navn', intl)}
+        value={navn}
+        onChange={(e) => settNavn(e.target.value)}
+      />
 
       <TextField
         label={hentTekst('person.ident', intl)}
         value={ident}
+        maxLength={11}
         onChange={(e) => settIdent(e.target.value)}
+        disabled={brukerIkkeIdent}
         error={
           visFeil ? hentTekst('person.feilmelding.ident', intl) : undefined
         }
       />
 
-      <CheckboxGroup
-        legend={hentTekst('person.checkbox.ident', intl)}
-        hideLegend
-        onChange={handleCheckboxChange}
+      <Checkbox
+        checked={brukerIkkeIdent}
+        onChange={(e) => {
+          const checked = e.target.checked;
+
+          settBrukerIkkeIdent(checked);
+
+          if (checked) {
+            settIdent('');
+          }
+        }}
       >
-        <Checkbox value="Ja">
-          {hentTekst('person.checkbox.ident', intl)}
-        </Checkbox>
-      </CheckboxGroup>
+        {hentTekst('person.checkbox.ident', intl)}
+      </Checkbox>
 
-      <DatePicker {...fødselsdato.datepickerProps}>
-        <DatePicker.Input
-          {...fødselsdato.inputProps}
-          label={hentTekst('datovelger.fødselsdato', intl)}
-        />
-      </DatePicker>
+      {skalViseFødselsdato && (
+        <DatePicker {...fødselsdato.datepickerProps}>
+          <DatePicker.Input
+            {...fødselsdato.inputProps}
+            label={hentTekst('datovelger.fødselsdato', intl)}
+          />
+        </DatePicker>
+      )}
 
-      <DatePicker {...flyttetFraDato.datepickerProps}>
-        <DatePicker.Input
-          {...flyttetFraDato.inputProps}
-          label={hentTekst('sivilstatus.datovelger.flyttetFraHverandre', intl)}
-        />
-      </DatePicker>
+      {skalViseFlyttedato && (
+        <DatePicker {...flyttetFraDato.datepickerProps}>
+          <DatePicker.Input
+            {...flyttetFraDato.inputProps}
+            label={hentTekst(
+              'sivilstatus.datovelger.flyttetFraHverandre',
+              intl
+            )}
+          />
+        </DatePicker>
+      )}
     </VStack>
   );
 };
