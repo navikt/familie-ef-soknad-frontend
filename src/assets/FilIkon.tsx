2646b1b2c8c70da583a64669c5144a56b1aec2e9
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-06-20 12:14:33 +0200

    Flyttet focusable fra path til svg

--- src/assets/FilIkon.tsx
+++ src/assets/FilIkon.tsx
@@ -13,12 +13,12 @@ export const FilIkon: React.FC<Props> = ({ className }) => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     role="img"
+    focusable="false"
   >
     <title>Vedlegg</title>
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      focusable="false"
       d="M14 0L21 7V24H3V0H14ZM12 2H5V22H19V8H12V2ZM15 17V19H7V17H15ZM17 13V15H7V13H17ZM13 9V11H7V9H13ZM14 2.83V6L17.17 5.999L14 2.83Z"
       fill="#3E3832"
     />
