0a4f0e5e35b02909f8992d964af2c56a7be32336
krikof-nav <kristian.kofoed@nav.no>
2025-06-30 12:59:12 +0200

    Fjerner ubrukt kode.

--- src/søknad/steg/1-omdeg/spørsmål/skalDuIkkeEndreAdresseIFolkeregisteretSpørsmål.ts
+++
@@ -1,6 +0,0 @@
-import { BaseSpørsmål } from '../../../../components/spørsmål/komponent/nyrefaktorering/Spørsmål';
-
-export const skalDuIkkeEndreAdresseIFolkeregisteretSpørsmål: BaseSpørsmål = {
-  id: 'skalDuIkkeEndreAdresseIFolkeregisteretSpørsmål',
-  spørsmålTekstKey: 'personopplysninger.info.endreAdresse',
-};
