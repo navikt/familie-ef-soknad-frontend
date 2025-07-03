d47add441e1113a7b3409ce0fb563e54f0cbbb89
krikof-nav <kristian.kofoed@nav.no>
2025-07-03 10:54:20 +0200

    Refaktorering.

--- src/components/spørsmål/input/RadioTile.test.tsx
+++ src/components/spørsmål/input/RadioTile.test.tsx
@@ -2,7 +2,7 @@ import { render } from '../../../test/render';
 import { RadioTile } from './RadioTile';
 
 describe('RadioTile komponent', () => {
-  test('Rendrer alternativer horisontalt', async () => {
+  test('Rendrer alternativer', async () => {
     const svarAlternativer = ['Ja', 'Nei'];
 
     const { screen } = render(
