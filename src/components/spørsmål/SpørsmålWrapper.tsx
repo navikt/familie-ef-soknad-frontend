d89514860ab4c4611bb7bbbf6b1cba762a9469fc
krikof-nav <kristian.kofoed@nav.no>
2025-07-15 15:48:30 +0200

    Viser nå årsak spørsmål, men jobber med utbedring.

---
+++ src/components/spørsmål/SpørsmålWrapper.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Heading, ReadMore, VStack } from '@navikt/ds-react';
+import styles from './SpørsmålWrapper.module.css';
+import { StegSpørsmål } from '../../models/felles/spørsmålogsvar';
+import { useLokalIntlContext } from '../../context/LokalIntlContext';
+import { hentTekst } from '../../utils/søknad';
+
+interface Props {
+  spørsmål: StegSpørsmål;
+  lesMerTittel?: string;
+  lesMerTekst?: string;
+}
+
+export const SpørsmålWrapper: React.FC<Props> = ({ spørsmål, lesMerTittel, lesMerTekst }) => {
+  const intl = useLokalIntlContext();
+
+  const spørsmålTekst = hentTekst(spørsmål.spørsmålKey, intl);
+
+  return (
+    <VStack gap="6">
+      <Heading size="xsmall" className={styles.heading}>
+        {spørsmålTekst}
+      </Heading>
+
+      {lesMerTittel && lesMerTekst && <ReadMore header={lesMerTittel}>{lesMerTekst}</ReadMore>}
+    </VStack>
+  );
+};
--- src/søknader/felles/steg/1-omdeg/sivilstatus/SivilstatusV2.tsx
+++ src/søknader/felles/steg/1-omdeg/sivilstatus/SivilstatusV2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLokalIntlContext } from '../../../../../context/LokalIntlContext';
 import { StegSpørsmål, SvarAlternativ } from '../../../../../models/felles/spørsmålogsvar';
 import {
@@ -7,29 +7,60 @@ import {
 } from '../../../../../components/spørsmål/JaNeiSpørsmålV2';
 import { Alert, VStack } from '@navikt/ds-react';
 import { hentTekst } from '../../../../../utils/søknad';
+import { SpørsmålWrapper } from '../../../../../components/spørsmål/SpørsmålWrapper';
+import { RadioSpørsmål } from '../../../../../components/spørsmål/RadioSpørsmål';
 
 export const SivilstatusV2: React.FC = () => {
   const intl = useLokalIntlContext();
 
   const søkerErGiftUtenRegistrering = useJaNeiBoolean();
   const søkerErSeparertEllerSkiltUtenRegistrering = useJaNeiBoolean();
+  const hvorforSøkerErAleneMedBarn = useState<SvarAlternativ | undefined>;
+
+  const hvorforSøkerErAleneMedBarnSvarAlternativer: SvarAlternativ[] = [
+    {
+      id: 'samlivsbruddMedDenAndreForelderen',
+      labelKey: 'sivilstatus.svar.samlivsbruddForeldre',
+    },
+    {
+      id: 'samlivsbruddMedNoenAndre',
+      labelKey: 'sivilstatus.svar.samlivsbruddAndre',
+    },
+    {
+      id: 'aleneFraFødsel',
+      labelKey: 'sivilstatus.svar.aleneFraFødsel',
+    },
+    {
+      id: 'endringIOmsorgForBarn',
+      labelKey: 'sivilstatus.svar.endringISamværsordning',
+    },
+    {
+      id: 'aleneGrunnetDødsfall',
+      labelKey: 'sivilstatus.svar.dødsfall',
+    },
+  ];
 
   const søkerErGiftUtenRegistreringSpørsmål: StegSpørsmål = {
     id: 'søkerErGiftUtenRegistrering',
     spørsmålKey: 'sivilstatus.spm.erUformeltGift',
   };
-
   const søkerErSeparertEllerSkiltUtenRegistreringSpørsmål: StegSpørsmål = {
     id: 'søkerErSeparertEllerSkiltUtenRegistrering',
     spørsmålKey: 'sivilstatus.spm.erUformeltSeparertEllerSkilt',
+  };
+  const hvorforErSøkerAleneMedBarnSpørsmål: StegSpørsmål = {
+    id: 'hvorforErSøkerAleneMedBarn',
+    spørsmålKey: 'sivilstatus.spm.begrunnelse',
   };
 
   const onSøkerErGiftUtenRegistrering = (svar: SvarAlternativ) => {
     søkerErGiftUtenRegistrering.handleChange(svar);
   };
-
   const onSøkerErSeparertEllerSkiltUtenRegistrering = (svar: SvarAlternativ) => {
     søkerErSeparertEllerSkiltUtenRegistrering.handleChange(svar);
+  };
+  const onHvorforSøkerErAleneMedBarn = (svar: SvarAlternativ) => {
+    // TODO: Fix me!
   };
 
   const visSøkerMåDokumentereEkteskapAlert = søkerErGiftUtenRegistrering.erJa;
@@ -37,6 +68,7 @@ export const SivilstatusV2: React.FC = () => {
     søkerErGiftUtenRegistrering.erBesvart;
   const visSøkerMåDokumentereSeperasjonEllerSkilsmisseAlert =
     søkerErSeparertEllerSkiltUtenRegistrering.erJa;
+  const visHvorforErSøkerAleneMedBarnSpørsmål = søkerErSeparertEllerSkiltUtenRegistrering.erBesvart;
 
   return (
     <VStack gap={'6'}>
@@ -67,6 +99,24 @@ export const SivilstatusV2: React.FC = () => {
               {hentTekst('sivilstatus.alert.erUformeltSeparertEllerSkilt', intl)}
             </Alert>
           )}
+        </VStack>
+      )}
+
+      {visHvorforErSøkerAleneMedBarnSpørsmål && (
+        <VStack gap={'6'}>
+          <SpørsmålWrapper
+            spørsmål={hvorforErSøkerAleneMedBarnSpørsmål}
+            lesMerTittel={hentTekst('sivilstatus.hjelpetekst-åpne.begrunnelse', intl)}
+            lesMerTekst={hentTekst('sivilstatus.hjelpetekst-innhold.begrunnelse', intl)}
+          />
+
+          <RadioSpørsmål
+            spørsmål={hvorforErSøkerAleneMedBarnSpørsmål}
+            svarAlternativer={hvorforSøkerErAleneMedBarnSvarAlternativer}
+            valgtVerdi={undefined}
+            svarLayout={'vertical'}
+            onChange={onHvorforSøkerErAleneMedBarn}
+          />
         </VStack>
       )}
     </VStack>
