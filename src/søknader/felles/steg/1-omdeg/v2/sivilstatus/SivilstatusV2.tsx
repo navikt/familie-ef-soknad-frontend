64829d3bfbba2205c7aad054de2681b89d857f12
krikof-nav <kristian.kofoed@nav.no>
2025-07-17 14:57:46 +0200

    AleneMedBarnÅrsak finnes nå, jobbes med å utbedre.

--- src/søknader/felles/steg/1-omdeg/v2/sivilstatus/SivilstatusV2.tsx
+++ src/søknader/felles/steg/1-omdeg/v2/sivilstatus/SivilstatusV2.tsx
@@ -7,6 +7,7 @@ import {
   erSøkerUGiftSkiltSeparertEllerEnke,
 } from '../../../../../../utils/sivilstatus';
 import { SøkerErSeparertEllerUgift } from './SøkerErSeparertEllerUgift';
+import { AleneMedBarnÅrsak } from './begrunnelse/AleneMedBarnÅrsak';
 
 export const SivilstatusV2: React.FC = () => {
   const { søker } = useOmDegV2();
@@ -14,12 +15,15 @@ export const SivilstatusV2: React.FC = () => {
 
   const visSøkerErGiftSpørsmål = erSøkerGift(sivilstand);
   const visSøkerErSeparertEllerSkiltSpørsmål = erSøkerUGiftSkiltSeparertEllerEnke(sivilstand);
+  const visÅrsakEnsligSpørsmål = true; // TODO: Fiks denne slik at vi viser den kun når sivilstand spørsmål er svart.
 
   return (
     <VStack gap={'6'}>
       {visSøkerErGiftSpørsmål && <SøkerErGiftV2 />}
 
       {visSøkerErSeparertEllerSkiltSpørsmål && <SøkerErSeparertEllerUgift />}
+
+      {visÅrsakEnsligSpørsmål && <AleneMedBarnÅrsak />}
     </VStack>
   );
 };
---
+++ src/søknader/felles/steg/1-omdeg/v2/sivilstatus/begrunnelse/AleneMedBarnÅrsak.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { StegSpørsmål, SvarAlternativ } from '../../typer/SpørsmålSvarStruktur';
+import { SpørsmålWrapper } from '../../../../../../../components/spørsmål/v2/SpørsmålWrapper';
+import { VStack } from '@navikt/ds-react';
+import { RadioSpørsmål } from '../../../../../../../components/spørsmål/v2/RadioSpørsmål';
+import { hentTekst } from '../../../../../../../utils/søknad';
+import { useLokalIntlContext } from '../../../../../../../context/LokalIntlContext';
+
+export const AleneMedBarnÅrsak: React.FC = () => {
+  const intl = useLokalIntlContext();
+
+  const [søkerAleneMedBarnÅrsak, settSøkerAleneMedBarnÅrsak] = useState<
+    SvarAlternativ | undefined
+  >();
+
+  const hvorforErDuAleneMedBarnSpørsmål: StegSpørsmål = {
+    id: 'årsakEnslig',
+    spørsmålKey: 'sivilstatus.spm.begrunnelse',
+  };
+
+  const hvorforAlenedMedBarnSvarAlternativer: SvarAlternativ[] = [
+    { id: 'samlivsbruddForeldre', svarKey: 'sivilstatus.svar.samlivsbruddForeldre' },
+    { id: 'samlivsbruddAndre', svarKey: 'sivilstatus.svar.samlivsbruddAndre' },
+    { id: 'endringISamværsordning', svarKey: 'sivilstatus.svar.endringISamværsordning' },
+    { id: 'aleneFraFødsel', svarKey: 'sivilstatus.svar.aleneFraFødsel' },
+    { id: 'dødsfall', svarKey: 'sivilstatus.svar.dødsfall' },
+  ];
+
+  const onÅrsakSøkerErAleneMedBarn = (svar: SvarAlternativ) => {
+    settSøkerAleneMedBarnÅrsak(svar);
+  };
+
+  return (
+    <VStack gap={'6'}>
+      <SpørsmålWrapper
+        spørsmål={hvorforErDuAleneMedBarnSpørsmål}
+        lesMerTittel={hentTekst('sivilstatus.hjelpetekst-åpne.begrunnelse', intl)}
+        lesMerTekst={hentTekst('sivilstatus.hjelpetekst-innhold.begrunnelse', intl)}
+      />
+
+      <RadioSpørsmål
+        spørsmål={hvorforErDuAleneMedBarnSpørsmål}
+        svarAlternativer={hvorforAlenedMedBarnSvarAlternativer}
+        valgtVerdi={undefined}
+        svarLayout={'vertical'}
+        onChange={onÅrsakSøkerErAleneMedBarn}
+      />
+    </VStack>
+  );
+};
