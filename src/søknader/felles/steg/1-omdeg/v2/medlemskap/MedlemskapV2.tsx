646a443e804ce7f0824fecf72c867f40f5b2446a
krikof-nav <kristian.kofoed@nav.no>
2025-07-22 10:58:42 +0200

    Flytter innhold fra OppholdMedBarn inn i MedlemskapV2.tsx midlertidlig for enklere state håndtering.

--- src/søknader/felles/steg/1-omdeg/v2/medlemskap/MedlemskapV2.tsx
+++ src/søknader/felles/steg/1-omdeg/v2/medlemskap/MedlemskapV2.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { VStack } from '@navikt/ds-react';
+import React, { useState } from 'react';
+import { Select, VStack } from '@navikt/ds-react';
 import {
   JaNeiSpørsmålV2,
   useJaNeiBoolean,
 } from '../../../../../../components/spørsmål/v2/JaNeiSpørsmålV2';
 import { StegSpørsmål, SvarAlternativ } from '../typer/SpørsmålSvarStruktur';
-import { OppholdMedBarn } from './OppholdMedBarn';
+import { useLokalIntlContext } from '../../../../../../context/LokalIntlContext';
+import { useSpråkContext } from '../../../../../../context/SpråkContext';
+import { hentLand } from '../../medlemskap/MedlemskapConfig';
+import { hentTekst } from '../../../../../../utils/søknad';
+import { ILandMedKode } from '../../../../../../models/steg/omDeg/medlemskap';
 
 export const MedlemskapV2: React.FC = () => {
+  const intl = useLokalIntlContext();
+  const [locale] = useSpråkContext();
+  const landListe = hentLand(locale);
+
   const søkerOppholderSegINorgeMedBarn = useJaNeiBoolean();
+  const [valgtLand, settValgtLand] = useState<string>('');
   const søkerHarOppholdtSegINorgeSiste5År = useJaNeiBoolean();
 
   const søkerOppholderSegINorgeMedBarnSpørsmål: StegSpørsmål = {
     id: 'søkerOppholderSegINorge',
     spørsmålKey: 'medlemskap.spm.opphold',
+  };
+  const hvorOppholderDuOgBarnSpørsmål: StegSpørsmål = {
+    id: 'oppholdsland',
+    spørsmålKey: 'medlemskap.spm.oppholdsland',
   };
   const søkerHarOppholdtSegINorgeSiste5ÅrSpørsmål: StegSpørsmål = {
     id: 'søkerBosattINorgeSisteTreÅr',
@@ -22,13 +35,17 @@ export const MedlemskapV2: React.FC = () => {
 
   const onSøkerOppholderSegILandMedBarn = (svar: SvarAlternativ) => {
     søkerOppholderSegINorgeMedBarn.handleChange(svar);
+    settValgtLand('');
+  };
+  const onLandEndring = (land: string) => {
+    settValgtLand(land);
   };
   const onSøkerHarOppholdtSegINorgeSiste5År = (svar: SvarAlternativ) => {
     søkerHarOppholdtSegINorgeSiste5År.handleChange(svar);
   };
 
   const visHvorOppholderDuOgBarnSpørsmål = søkerOppholderSegINorgeMedBarn.erNei;
-  const visOppholdINorgeSiste5ÅrSpørsmål = true;
+  const visOppholdINorgeSiste5ÅrSpørsmål = valgtLand.trim() !== '';
 
   return (
     <VStack gap={'6'}>
@@ -37,7 +54,25 @@ export const MedlemskapV2: React.FC = () => {
         onChange={onSøkerOppholderSegILandMedBarn}
       />
 
-      {visHvorOppholderDuOgBarnSpørsmål && <OppholdMedBarn />}
+      {visHvorOppholderDuOgBarnSpørsmål && (
+        <Select
+          label={hentTekst(hvorOppholderDuOgBarnSpørsmål.spørsmålKey, intl)}
+          value={valgtLand}
+          onChange={(event) => {
+            onLandEndring(event.target.value);
+          }}
+        >
+          <option value="" disabled>
+            {hentTekst('landVelger.alternativ', intl)}
+          </option>
+
+          {landListe.map((land: ILandMedKode) => (
+            <option key={land.id} value={land.id}>
+              {land.svar_tekst}
+            </option>
+          ))}
+        </Select>
+      )}
 
       {visOppholdINorgeSiste5ÅrSpørsmål && (
         <JaNeiSpørsmålV2
