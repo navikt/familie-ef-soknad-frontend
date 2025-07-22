1b683ed891dd16507b0d5bd655b6eccad7403f4f
krikof-nav <kristian.kofoed@nav.no>
2025-07-22 10:54:35 +0200

    Jobber med å utbedre MedlemskapV2.tsx. Fortsatt WIP.

--- src/søknader/felles/steg/1-omdeg/v2/medlemskap/MedlemskapV2.tsx
+++ src/søknader/felles/steg/1-omdeg/v2/medlemskap/MedlemskapV2.tsx
@@ -9,17 +9,26 @@ import { OppholdMedBarn } from './OppholdMedBarn';
 
 export const MedlemskapV2: React.FC = () => {
   const søkerOppholderSegINorgeMedBarn = useJaNeiBoolean();
+  const søkerHarOppholdtSegINorgeSiste5År = useJaNeiBoolean();
 
   const søkerOppholderSegINorgeMedBarnSpørsmål: StegSpørsmål = {
     id: 'søkerOppholderSegINorge',
     spørsmålKey: 'medlemskap.spm.opphold',
   };
+  const søkerHarOppholdtSegINorgeSiste5ÅrSpørsmål: StegSpørsmål = {
+    id: 'søkerBosattINorgeSisteTreÅr',
+    spørsmålKey: 'medlemskap.spm.bosatt',
+  };
 
   const onSøkerOppholderSegILandMedBarn = (svar: SvarAlternativ) => {
     søkerOppholderSegINorgeMedBarn.handleChange(svar);
   };
+  const onSøkerHarOppholdtSegINorgeSiste5År = (svar: SvarAlternativ) => {
+    søkerHarOppholdtSegINorgeSiste5År.handleChange(svar);
+  };
 
   const visHvorOppholderDuOgBarnSpørsmål = søkerOppholderSegINorgeMedBarn.erNei;
+  const visOppholdINorgeSiste5ÅrSpørsmål = true;
 
   return (
     <VStack gap={'6'}>
@@ -29,6 +38,13 @@ export const MedlemskapV2: React.FC = () => {
       />
 
       {visHvorOppholderDuOgBarnSpørsmål && <OppholdMedBarn />}
+
+      {visOppholdINorgeSiste5ÅrSpørsmål && (
+        <JaNeiSpørsmålV2
+          spørsmål={søkerHarOppholdtSegINorgeSiste5ÅrSpørsmål}
+          onChange={onSøkerHarOppholdtSegINorgeSiste5År}
+        />
+      )}
     </VStack>
   );
 };
