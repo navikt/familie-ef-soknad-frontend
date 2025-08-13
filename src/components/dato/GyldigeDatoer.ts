32e7e1b1571466f3926e464e56a94f5d6fc3c4be
krikof-nav <kristian.kofoed@nav.no>
2025-08-13 13:02:24 +0200

    Legger til kommentar som beskriver hensikt av GyldigeDatoer enum.

--- src/components/dato/GyldigeDatoer.ts
+++ src/components/dato/GyldigeDatoer.ts
@@ -1,3 +1,11 @@
+/**
+ * Enum som definerer hvilke datoer som er gyldige i ulike kontekster.
+ * Brukes ofte til validering av datoer i datovelgere (f.eks. fødselsdato, flyttedato).
+ * Verdiene representerer ulike intervaller som er tillatt, som fremtidige datoer,
+ * tidligere datoer, eller kombinasjoner av historiske og fremtidige datoer.
+ * Ofte begrenser dette datovelgerene fra å la bruker setter verdier utenfor grensen.
+ */
+
 export enum GyldigeDatoer {
   alle = 'alle',
   fremtidige = 'fremtidige',
