801d6055f44bbde78dfc6f5a5d7bf66a4265fe84
krikof-nav <kristian.kofoed@nav.no>
2025-08-27 09:30:20 +0200

    Utbedrer visningskondisjonal for spørsmål i BosituasjonSpørsmål.tsx. Fjerner også seksjonsgruppe fra mange av komponentene.

--- src/søknader/felles/steg/2-bosituasjon/BosituasjonSpørsmål.tsx
+++ src/søknader/felles/steg/2-bosituasjon/BosituasjonSpørsmål.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import SeksjonGruppe from '../../../../components/gruppe/SeksjonGruppe';
 import MultiSvarSpørsmål from '../../../../components/spørsmål/MultiSvarSpørsmål';
 import FeltGruppe from '../../../../components/gruppe/FeltGruppe';
 import AlertStripeDokumentasjon from '../../../../components/AlertstripeDokumentasjon';
@@ -9,23 +8,20 @@ import { EkteskapsliknendeForhold } from './EkteskapsliknendeForhold';
 import { OmTidligereSamboer } from './OmTidligereSamboer';
 import { ISvar } from '../../../../models/felles/spørsmålogsvar';
 import { erValgtSvarLiktSomSvar, harValgtSvar } from '../../../../utils/spørsmålogsvar';
-import { Alert } from '@navikt/ds-react';
+import { Alert, VStack } from '@navikt/ds-react';
 import { useBosituasjon } from './BosituasjonContext';
 import { hentHTMLTekst, hentTekst } from '../../../../utils/teksthåndtering';
 import { useLokalIntlContext } from '../../../../context/LokalIntlContext';
 
 export const BosituasjonSpørsmål: FC = () => {
-  const { bosituasjon, oppdaterDelerBoligMedAndreVoksne, hovedSpørsmål } = useBosituasjon();
   const intl = useLokalIntlContext();
 
+  const { bosituasjon, oppdaterDelerBoligMedAndreVoksne, hovedSpørsmål } = useBosituasjon();
   const { delerBoligMedAndreVoksne, samboerDetaljer, datoFlyttetFraHverandre } = bosituasjon;
 
   const valgtSvar: ISvar | undefined = hovedSpørsmål.svaralternativer.find((svar) =>
     erValgtSvarLiktSomSvar(delerBoligMedAndreVoksne.verdi, svar.svar_tekst)
   );
-
-  const harEkteskapsliknendeForhold =
-    delerBoligMedAndreVoksne.svarid === ESøkerDelerBolig.harEkteskapsliknendeForhold;
 
   const harFyltUtIdentEllerKjennerIkkeIdentPåTidligereSamboer =
     samboerDetaljer?.kjennerIkkeIdent || harValgtSvar(samboerDetaljer?.ident?.verdi);
@@ -37,50 +33,45 @@ export const BosituasjonSpørsmål: FC = () => {
     harFyltUtIdentEllerKjennerIkkeIdentPåTidligereSamboer &&
     harValgtSvar(datoFlyttetFraHverandre?.verdi);
 
+  const visOmTidligereSamboer =
+    delerBoligMedAndreVoksne.svarid ===
+    ESøkerDelerBolig.tidligereSamboerFortsattRegistrertPåAdresse;
   const visPlanerOmÅFlytteSammenEllerFåSamboer =
     delerBoligMedAndreVoksne.svarid === ESøkerDelerBolig.borAleneMedBarnEllerGravid ||
     delerBoligMedAndreVoksne.svarid === ESøkerDelerBolig.delerBoligMedAndreVoksne ||
     ferdigUtfyltTidligereSamboerFortsattRegistrertPåAdresse;
+  const visEkteskapLignendeForhold =
+    delerBoligMedAndreVoksne.svarid === ESøkerDelerBolig.harEkteskapsliknendeForhold;
 
   return (
-    <>
-      <SeksjonGruppe>
-        <MultiSvarSpørsmål
-          key={hovedSpørsmål.søknadid}
-          spørsmål={hovedSpørsmål}
-          valgtSvar={delerBoligMedAndreVoksne.verdi}
-          settSpørsmålOgSvar={oppdaterDelerBoligMedAndreVoksne}
-        />
-        {valgtSvar && valgtSvar.alert_tekstid && (
-          <FeltGruppe>
-            {delerBoligMedAndreVoksne.svarid ===
-            ESøkerDelerBolig.tidligereSamboerFortsattRegistrertPåAdresse ? (
-              <AlertStripeDokumentasjon>
-                {hentHTMLTekst(valgtSvar.alert_tekstid, intl)}
-              </AlertStripeDokumentasjon>
-            ) : (
-              <Alert size="small" variant="warning" inline>
-                {hentTekst(valgtSvar.alert_tekstid, intl)}
-              </Alert>
-            )}
-          </FeltGruppe>
-        )}
-      </SeksjonGruppe>
+    <VStack>
+      <MultiSvarSpørsmål
+        key={hovedSpørsmål.søknadid}
+        spørsmål={hovedSpørsmål}
+        valgtSvar={delerBoligMedAndreVoksne.verdi}
+        settSpørsmålOgSvar={oppdaterDelerBoligMedAndreVoksne}
+      />
 
-      {delerBoligMedAndreVoksne.svarid ===
-        ESøkerDelerBolig.tidligereSamboerFortsattRegistrertPåAdresse && (
-        <SeksjonGruppe>
-          <OmTidligereSamboer />
-        </SeksjonGruppe>
+      {valgtSvar && valgtSvar.alert_tekstid && (
+        <FeltGruppe>
+          {delerBoligMedAndreVoksne.svarid ===
+          ESøkerDelerBolig.tidligereSamboerFortsattRegistrertPåAdresse ? (
+            <AlertStripeDokumentasjon>
+              {hentHTMLTekst(valgtSvar.alert_tekstid, intl)}
+            </AlertStripeDokumentasjon>
+          ) : (
+            <Alert size="small" variant="warning" inline>
+              {hentTekst(valgtSvar.alert_tekstid, intl)}
+            </Alert>
+          )}
+        </FeltGruppe>
       )}
 
-      {visPlanerOmÅFlytteSammenEllerFåSamboer && (
-        <SeksjonGruppe>
-          <SøkerSkalFlytteSammenEllerFåSamboer />
-        </SeksjonGruppe>
-      )}
+      {visOmTidligereSamboer && <OmTidligereSamboer />}
 
-      {harEkteskapsliknendeForhold && <EkteskapsliknendeForhold />}
-    </>
+      {visPlanerOmÅFlytteSammenEllerFåSamboer && <SøkerSkalFlytteSammenEllerFåSamboer />}
+
+      {visEkteskapLignendeForhold && <EkteskapsliknendeForhold />}
+    </VStack>
   );
 };
