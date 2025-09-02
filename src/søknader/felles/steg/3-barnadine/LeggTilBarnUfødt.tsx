933b703520af76bca1bf7020cd6748ef6b79a150
krikof-nav <kristian.kofoed@nav.no>
2025-09-02 15:25:47 +0200

    Fjerner komponent gruppe og style div fra LeggTilBarnUfødt.tsx

--- src/søknader/felles/steg/3-barnadine/LeggTilBarnUfødt.tsx
+++ src/søknader/felles/steg/3-barnadine/LeggTilBarnUfødt.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import KomponentGruppe from '../../../../components/gruppe/KomponentGruppe';
 import { ESvar, ESvarTekstid } from '../../../../models/felles/spørsmålogsvar';
 import { hentTekst } from '../../../../utils/teksthåndtering';
 import AlertStripeDokumentasjon from '../../../../components/AlertstripeDokumentasjon';
 import { erDatoGyldigOgInnenforBegrensning } from '../../../../utils/gyldigeDatoerUtils';
 import { useLokalIntlContext } from '../../../../context/LokalIntlContext';
-import { Alert, RadioGroup } from '@navikt/ds-react';
-import styled from 'styled-components';
+import { Alert, RadioGroup, VStack } from '@navikt/ds-react';
+import styles from './LeggTilBarnUfødt.module.css';
 import { Datovelger } from '../../../../components/dato/Datovelger';
 import RadioPanelCustom from '../../../../components/panel/RadioPanel';
 import { GyldigeDatoer } from '../../../../components/dato/GyldigeDatoer';
@@ -18,29 +17,12 @@ interface Props {
   barnDato: string;
 }
 
-const RadiopanelWrapper = styled.div`
-  .navds-fieldset .navds-radio-buttons {
-    margin-top: 0;
-  }
-  .navds-radio-buttons {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-auto-rows: min-content;
-    grid-gap: 1rem;
-    padding-top: 1rem;
-
-    @media all and (max-width: 767px) {
-      grid-template-columns: 1fr;
-    }
-  }
-`;
-
 export const LeggTilBarnUfødt: React.FC<Props> = ({ settBo, boHosDeg, settDato, barnDato }) => {
   const intl = useLokalIntlContext();
 
   return (
-    <>
-      <KomponentGruppe>
+    <VStack gap="6">
+      <VStack gap="4">
         <Datovelger
           settDato={(e) => settDato(e)}
           valgtDato={barnDato}
@@ -50,10 +32,10 @@ export const LeggTilBarnUfødt: React.FC<Props> = ({ settBo, boHosDeg, settDato,
         <AlertStripeDokumentasjon>
           {hentTekst('barnadine.info.terminbekreftelse', intl)}
         </AlertStripeDokumentasjon>
-      </KomponentGruppe>
+      </VStack>
       {barnDato && erDatoGyldigOgInnenforBegrensning(barnDato, GyldigeDatoer.Fremtidige) && (
-        <KomponentGruppe>
-          <RadiopanelWrapper>
+        <VStack gap="4">
+          <div className={styles.radiopanelWrapper}>
             <RadioGroup
               legend={hentTekst('barnekort.spm.skalBarnetBoHosSøker', intl)}
               value={boHosDeg}
@@ -77,14 +59,14 @@ export const LeggTilBarnUfødt: React.FC<Props> = ({ settBo, boHosDeg, settDato,
                 {hentTekst(ESvarTekstid.NEI, intl)}
               </RadioPanelCustom>
             </RadioGroup>
-          </RadiopanelWrapper>
+          </div>
           {boHosDeg === ESvar.NEI && (
             <Alert size="small" variant="warning" inline>
               {hentTekst('barnadine.advarsel.skalikkebo', intl)}
             </Alert>
           )}
-        </KomponentGruppe>
+        </VStack>
       )}
-    </>
+    </VStack>
   );
 };
