01bf9f1e730f11eec897637d0e265c5d44d70064
krikof <kristian.kofoed@nav.no>
2025-09-04 11:45:21 +0200

    Revert "Fra texas til oasis (#2003)" (#2005)
    This reverts commit 1b67d79a37bc8b4779845cd916528a15a62ee794.

--- src/søknader/skolepenger/SkolepengerApp.tsx
+++ src/søknader/skolepenger/SkolepengerApp.tsx
@@ -24,6 +24,7 @@ const SkolepengerApp = () => {
   const { søknad, settSøknad, hentMellomlagretSkolepenger } = useSkolepengerSøknad();
   const { settToggles } = useToggles();
   const intl = useLokalIntlContext();
+
   autentiseringsInterceptor();
 
   useEffect(() => {
