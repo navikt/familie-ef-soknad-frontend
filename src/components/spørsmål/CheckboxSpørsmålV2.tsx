1b553640f434df8fd82d5dbf156cd121b11f946d
krikof-nav <kristian.kofoed@nav.no>
2025-09-08 14:52:09 +0200

    Prøver å legge til ReadMore under CheckboxGroup for å se om dette fortsatt skaper grouping problem.

--- src/components/spørsmål/CheckboxSpørsmålV2.tsx
+++ src/components/spørsmål/CheckboxSpørsmålV2.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ISpørsmål, ISvar } from '../../models/felles/spørsmålogsvar';
 import { useLokalIntlContext } from '../../context/LokalIntlContext';
-import { Checkbox, CheckboxGroup, Heading, ReadMore, VStack } from '@navikt/ds-react';
+import { Checkbox, CheckboxGroup, ReadMore } from '@navikt/ds-react';
 import { hentTekst } from '../../utils/teksthåndtering';
 import styles from './CheckboxSpørsmålV2.module.css';
 
@@ -21,37 +21,40 @@ export const CheckboxSpørsmålV2: React.FC<Props> = ({
   const intl = useLokalIntlContext();
 
   return (
-    <VStack gap={'6'}>
-      <VStack gap={'2'}>
-        <Heading size={'xsmall'}>{hentTekst(spørsmål.tekstid, intl)}</Heading>
-
-        {spørsmål.lesmer && (
+    <CheckboxGroup
+      legend={hentTekst(spørsmål.tekstid, intl)}
+      value={valgteSvar}
+      description={
+        spørsmål.lesmer && (
           <ReadMore header={hentTekst(spørsmål.lesmer.headerTekstid, intl)} size={'small'}>
             {hentTekst(spørsmål.lesmer.innholdTekstid, intl)}
           </ReadMore>
-        )}
-      </VStack>
+        )
+      }
+    >
+      {spørsmål.svaralternativer.map((svar) => {
+        const alleredeHuketAvISøknad = valgteSvar.some((valgtSvar) => {
+          return valgtSvar === svar.svar_tekst || valgtSvar === svar.id;
+        });
 
-      <CheckboxGroup legend={hentTekst(spørsmål.tekstid, intl)} onChange={() => {}} hideLegend>
-        {spørsmål.svaralternativer.map((svar) => {
-          const verdi = brukSvarIdSomVerdi ? svar.id : svar.svar_tekst;
-          const erValgt = valgteSvar.includes(verdi);
-
-          return (
-            <Checkbox
-              key={svar.id}
-              value={verdi}
-              checked={erValgt}
-              onChange={(e) => settValgteSvar(spørsmål, e.target.checked, svar)}
-              className={
-                erValgt ? `${styles.checkboxPanel} ${styles.active}` : styles.checkboxPanel
-              }
-            >
-              {hentTekst(svar.svar_tekst, intl)}
-            </Checkbox>
-          );
-        })}
-      </CheckboxGroup>
-    </VStack>
+        return (
+          <Checkbox
+            key={svar.svar_tekst}
+            value={brukSvarIdSomVerdi ? svar.id : svar.svar_tekst}
+            checked={alleredeHuketAvISøknad}
+            onChange={() => {
+              settValgteSvar(spørsmål, alleredeHuketAvISøknad, svar);
+            }}
+            className={
+              alleredeHuketAvISøknad
+                ? `${styles.checkboxPanel} ${styles.active}`
+                : styles.checkboxPanel
+            }
+          >
+            {hentTekst(svar.svar_tekst, intl)}
+          </Checkbox>
+        );
+      })}
+    </CheckboxGroup>
   );
 };
