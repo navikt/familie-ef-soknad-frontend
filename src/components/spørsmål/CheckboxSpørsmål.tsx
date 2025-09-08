49552ee80760198e546a6a107befaf4b60499c9f
krikof-nav <kristian.kofoed@nav.no>
2025-09-08 15:20:43 +0200

    V2 er nå i CheckboxSpørsmål.tsx

--- src/components/spørsmål/CheckboxSpørsmål.tsx
+++ src/components/spørsmål/CheckboxSpørsmål.tsx
@@ -1,34 +1,15 @@
 import React from 'react';
 import { ISpørsmål, ISvar } from '../../models/felles/spørsmålogsvar';
-import styled from 'styled-components';
-import LesMerTekst from '../LesMerTekst';
 import { useLokalIntlContext } from '../../context/LokalIntlContext';
-import CheckboxPanelCustom from '../panel/CheckboxPanel';
-import { CheckboxGroup } from '@navikt/ds-react';
+import { Checkbox, CheckboxGroup, ReadMore } from '@navikt/ds-react';
 import { hentTekst } from '../../utils/teksthåndtering';
+import styles from './CheckboxSpørsmål.module.css';
 
-const StyledCheckboxSpørsmål = styled.div`
-  .navds-fieldset .navds-checkboxes {
-    margin-top: 0;
-  }
-  .navds-checkboxes {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-auto-rows: min-content;
-    grid-gap: 1rem;
-    padding-top: 1rem;
-
-    @media all and (max-width: 420px) {
-      grid-template-columns: 1fr;
-    }
-  }
-`;
 
 interface Props {
   spørsmål: ISpørsmål;
   settValgteSvar: (spørsmål: ISpørsmål, svarHuketAv: boolean, svar: ISvar) => void;
   valgteSvar: string[];
-  skalLogges: boolean;
   brukSvarIdSomVerdi?: boolean;
 }
 
@@ -41,39 +22,41 @@ const CheckboxSpørsmål: React.FC<Props> = ({
   const intl = useLokalIntlContext();
 
   return (
-    <StyledCheckboxSpørsmål key={spørsmål.søknadid}>
-      <CheckboxGroup
-        legend={hentTekst(spørsmål.tekstid, intl)}
-        value={valgteSvar}
-        description={
-          spørsmål.lesmer && (
-            <LesMerTekst
-              åpneTekstid={spørsmål.lesmer.headerTekstid}
-              innholdTekstid={spørsmål.lesmer.innholdTekstid}
-            />
-          )
-        }
-      >
-        {spørsmål.svaralternativer.map((svar: ISvar) => {
-          const alleredeHuketAvISøknad = valgteSvar.some((valgtSvar) => {
-            return valgtSvar === svar.svar_tekst || valgtSvar === svar.id;
-          });
+    <CheckboxGroup
+      legend={hentTekst(spørsmål.tekstid, intl)}
+      value={valgteSvar}
+      description={
+        spørsmål.lesmer && (
+          <ReadMore header={hentTekst(spørsmål.lesmer.headerTekstid, intl)} size={'small'}>
+            {hentTekst(spørsmål.lesmer.innholdTekstid, intl)}
+          </ReadMore>
+        )
+      }
+    >
+      {spørsmål.svaralternativer.map((svar) => {
+        const alleredeHuketAvISøknad = valgteSvar.some((valgtSvar) => {
+          return valgtSvar === svar.svar_tekst || valgtSvar === svar.id;
+        });
 
-          return (
-            <CheckboxPanelCustom
-              key={svar.svar_tekst}
-              value={brukSvarIdSomVerdi ? svar.id : svar.svar_tekst}
-              checked={alleredeHuketAvISøknad}
-              onChange={() => {
-                settValgteSvar(spørsmål, alleredeHuketAvISøknad, svar);
-              }}
-            >
-              {svar.svar_tekst}
-            </CheckboxPanelCustom>
-          );
-        })}
-      </CheckboxGroup>
-    </StyledCheckboxSpørsmål>
+        return (
+          <Checkbox
+            key={svar.svar_tekst}
+            value={brukSvarIdSomVerdi ? svar.id : svar.svar_tekst}
+            checked={alleredeHuketAvISøknad}
+            onChange={() => {
+              settValgteSvar(spørsmål, alleredeHuketAvISøknad, svar);
+            }}
+            className={
+              alleredeHuketAvISøknad
+                ? `${styles.checkboxPanel} ${styles.active}`
+                : styles.checkboxPanel
+            }
+          >
+            {hentTekst(svar.svar_tekst, intl)}
+          </Checkbox>
+        );
+      })}
+    </CheckboxGroup>
   );
 };
 
