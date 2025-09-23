aec235796cc5c7329ae1b7088b3913fd955f7545
krikof-nav <kristian.kofoed@nav.no>
2025-09-23 11:59:46 +0200

    Legger til AktivitetContext for Aktivtet i overgangstønad. Jobber med å ta i bruk metodene.

---
+++ src/søknader/overgangsstønad/steg/5-aktivitet/AktivitetContext.ts
@@ -0,0 +1,40 @@
+import { SøknadOvergangsstønad } from '../../models/søknad';
+import { SøknadBarnetilsyn } from '../../../barnetilsyn/models/søknad';
+import { ISpørsmål, ISvar } from '../../../../models/felles/spørsmålogsvar';
+import constate from 'constate';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { IAktivitet } from '../../../../models/steg/aktivitet/aktivitet';
+
+export interface Props {
+  søknad: SøknadOvergangsstønad;
+  oppdaterSøknad: (søknad: SøknadOvergangsstønad) => void;
+  mellomlagreSøknad: (steg: string, oppdatertSøknad: SøknadBarnetilsyn) => void;
+  settDokumentasjonsbehov: (spørsmål: ISpørsmål, valgtSvar: ISvar, erHuketAv?: boolean) => void;
+}
+
+export const [AktivitetProvider, useAktivitet] = constate(
+  ({ søknad, oppdaterSøknad, mellomlagreSøknad, settDokumentasjonsbehov }: Props) => {
+    const location = useLocation();
+
+    const [aktivitet, settAktivitet] = useState<IAktivitet>(søknad?.aktivitet);
+
+    const mellomlagreSteg = () => {
+      const oppdatertAktivitet = { ...aktivitet };
+
+      const oppdatertSøknad = { ...søknad, aktivitet: oppdatertAktivitet };
+
+      oppdaterSøknad(oppdatertSøknad);
+
+      return mellomlagreSøknad(location.pathname, oppdatertSøknad);
+    };
+
+    return {
+      aktivitet,
+      settAktivitet,
+      søknad,
+      mellomlagreSteg,
+      settDokumentasjonsbehov,
+    };
+  }
+);
