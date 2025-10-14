2baeec2c77b9ec480a343dbc9fbdca30d6373118
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-10-14 18:43:04 +0200

    Fjernet sc fra veilederboks

--- src/components/forside/VeilederBoks.tsx
+++ src/components/forside/VeilederBoks.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import VeilederSnakkeboble from '../../assets/VeilederSnakkeboble';
 import { useLokalIntlContext } from '../../context/LokalIntlContext';
 import { usePersonContext } from '../../context/PersonContext';
-import styled from 'styled-components';
 import { hentTekstMedEnVariabel } from '../../utils/teksthåndtering';
-
-const VeilederContainer = styled.div`
-  margin-bottom: 4rem;
-`;
 
 export interface VeilederBoksProps {
   navn?: string;
@@ -19,8 +14,8 @@ export const VeilederBoks: React.FC<VeilederBoksProps> = ({ navn }) => {
 
   const søkerNavn = navn ? navn : person.søker.forkortetNavn;
   return (
-    <VeilederContainer>
+    <div style={{ marginBottom: '4rem' }}>
       <VeilederSnakkeboble tekst={hentTekstMedEnVariabel('skjema.hei', intl, søkerNavn)} />
-    </VeilederContainer>
+    </div>
   );
 };
