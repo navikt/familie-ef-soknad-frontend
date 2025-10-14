f3ab3940e3a21a434ab4257ea6f95737114efec1
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-10-14 18:04:17 +0200

    Småfiks på disclaimerboks og bruker native aksel i overgangsstønadinfo

--- src/components/forside/DisclaimerBoks.tsx
+++ src/components/forside/DisclaimerBoks.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Box, Checkbox, Heading } from '@navikt/ds-react';
+import { BodyShort, Box, Checkbox, Heading, VStack } from '@navikt/ds-react';
 import { hentTekst, hentTekstMedEnVariabel } from '../../utils/teksthåndtering';
 import React from 'react';
 import { useLokalIntlContext } from '../../context/LokalIntlContext';
@@ -11,23 +11,22 @@ export const DisclaimerBoks: React.FC<{
 }> = ({ navn, tekst, harBekreftet, settBekreftelse }) => {
   const intl = useLokalIntlContext();
   return (
-    <>
+    <VStack gap={'2'}>
       <Heading level="2" size="small">
         {hentTekst('skjema.forside.disclaimer.tittel', intl)}
       </Heading>
-      <br />
       <Box
-        background={'surface-warning-subtle'}
+        background={harBekreftet ? 'surface-success-subtle' : 'surface-warning-subtle'}
         padding={'4'}
         borderWidth={'1'}
         borderRadius={'medium'}
-        borderColor={'border-warning'}
+        borderColor={harBekreftet ? 'border-success' : 'border-warning'}
       >
         <BodyShort>{hentTekst(tekst, intl)}</BodyShort>
         <Checkbox checked={!!harBekreftet} onChange={() => settBekreftelse(!harBekreftet)}>
           {hentTekstMedEnVariabel('side.bekreftelse', intl, navn)}
         </Checkbox>
       </Box>
-    </>
+    </VStack>
   );
 };
