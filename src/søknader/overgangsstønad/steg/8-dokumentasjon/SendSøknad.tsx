db57942afbda3930bba716a182cea3f620ae623d
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-11-21 16:19:20 +0100

    Bytter til vanlig console.log

--- src/søknader/overgangsstønad/steg/8-dokumentasjon/SendSøknad.tsx
+++ src/søknader/overgangsstønad/steg/8-dokumentasjon/SendSøknad.tsx
@@ -19,7 +19,6 @@ import { oppdaterBarnLabels } from '../../../../utils/barn';
 import { Alert, BodyShort, Button, HStack } from '@navikt/ds-react';
 import { validerSøkerBosattINorgeSisteFemÅr } from '../../../../helpers/steg/omdeg';
 import { hentTekst } from '../../../../utils/teksthåndtering';
-import logger from '../../../../../server/logger';
 
 interface Innsending {
   status: string;
@@ -58,7 +57,7 @@ export const SendSøknadKnapper: FC = () => {
       });
       navigate(nesteRoute.path);
     } catch (e: any) {
-      logger.info(e);
+      console.log(e);
       settinnsendingState({
         ...innsendingState,
         status: IStatus.FEILET,
