38b005cb2894c0b320550c3c22f9a76d88524356
krikof-nav <kristian.kofoed@nav.no>
2026-01-15 10:22:30 +0100

    * Quick fix for feil navigasjon feil, der bruker blir navigert til gjenbruk dialog når bruker trykker tilbake uten å ha sendt inn tidligere søknad og gjenbruk er skrudd av. Dette skyldes at tilbake knappen navigerer til forrige route uten å skjekke gjenbruk relatert logikk. Dette legger til en quick fix som er ment å fikse ordentlig.

--- src/søknader/barnetilsyn/steg/0.5-gjenbruk/Gjenbruk.tsx
+++ src/søknader/barnetilsyn/steg/0.5-gjenbruk/Gjenbruk.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ERouteBarnetilsyn, RoutesBarnetilsyn } from '../../routing/routesBarnetilsyn';
 import { useLokalIntlContext } from '../../../../context/LokalIntlContext';
 import { BodyShort, Box, GuidePanel } from '@navikt/ds-react';
@@ -9,9 +10,26 @@ import styled from 'styled-components';
 import { KnappLocaleTekstOgNavigate } from '../../../../components/knapper/KnappLocaleTekstOgNavigate';
 import { hentTekst, hentTekstMedEnVariabel } from '../../../../utils/teksthåndtering';
 
+// TODO: Skrur av GJENBRUK midlertidlig til vi har løst mapping problematikk
+const ENABLE_GJENBRUK_FORRIGE_SØKNAD = false;
+
 const Gjenbruk: FC = () => {
   const intl = useLokalIntlContext();
   const { søknad } = useBarnetilsynSøknad();
+  const navigate = useNavigate();
+
+  // TODO: Skrur av GJENBRUK midlertidlig til vi har løst mapping problematikk
+  // TODO: Denne quickfixen forsikrer oss om at gjenbruk modalen ikke dukker opp.
+  // TODO: Dette skal fjernes.
+  useEffect(() => {
+    if (!ENABLE_GJENBRUK_FORRIGE_SØKNAD) {
+      navigate('/barnetilsyn', { replace: true });
+    }
+  }, [navigate]);
+
+  if (!ENABLE_GJENBRUK_FORRIGE_SØKNAD) {
+    return null;
+  }
 
   const BodyShortContainer = styled.div`
     & > *:not(:last-child) {
