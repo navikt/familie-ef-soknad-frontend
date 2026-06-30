b88fddf76364bead1658ce1e84ad5da08e237574
Robin Lund Sadun <47403589+RobinLundSadun@users.noreply.github.com>
2026-06-30 10:46:52 +0200

    Legger til varsel for barnetilsyn om regelendringer (#2264)

--- src/søknader/overgangsstønad/VarselRegelendring2026.tsx
+++ src/søknader/overgangsstønad/VarselRegelendring2026.tsx
@@ -2,24 +2,40 @@ import React from 'react';
 import { Alert, BodyLong, Heading } from '@navikt/ds-react';
 import { useLokalIntlContext } from '../../context/LokalIntlContext';
 import { hentTekst } from '../../utils/teksthåndtering';
+import { Stønadstype } from '../../models/søknad/stønadstyper';
 
-export const VarselRegelendring2026 = () => {
+interface Props {
+  stønadstype: Stønadstype;
+}
+
+export const VarselRegelendring2026 = (props: Props) => {
   const intl = useLokalIntlContext();
+
+  const tittelNøkkel =
+    props.stønadstype == Stønadstype.overgangsstønad
+      ? 'forside.overgangsstønad.varselRegelendring2026.tittel'
+      : 'forside.barnetilsyn.varselRegelendring2026.tittel';
+  const førsteAvsnittNøkkel =
+    props.stønadstype == Stønadstype.overgangsstønad
+      ? 'forside.overgangsstønad.varselRegelendring2026.førsteAvsnitt'
+      : 'forside.barnetilsyn.varselRegelendring2026.førsteAvsnitt';
+  const andreAvsnittNøkkel =
+    props.stønadstype == Stønadstype.overgangsstønad
+      ? 'forside.overgangsstønad.varselRegelendring2026.andreAvsnitt'
+      : 'forside.barnetilsyn.varselRegelendring2026.andreAvsnitt';
+  const tredjeAvsnittNøkkel =
+    props.stønadstype == Stønadstype.overgangsstønad
+      ? 'forside.overgangsstønad.varselRegelendring2026.tredjeAvsnitt'
+      : 'forside.barnetilsyn.varselRegelendring2026.tredjeAvsnitt';
 
   return (
     <Alert variant="info">
       <Heading spacing size="small" level="3">
-        {hentTekst('forside.overgangsstønad.varselRegelendring2026.tittel', intl)}
+        {hentTekst(tittelNøkkel, intl)}
       </Heading>
-      <BodyLong spacing>
-        {hentTekst('forside.overgangsstønad.varselRegelendring2026.førsteAvsnitt', intl)}
-      </BodyLong>
-      <BodyLong spacing>
-        {hentTekst('forside.overgangsstønad.varselRegelendring2026.andreAvsnitt', intl)}
-      </BodyLong>
-      <BodyLong spacing>
-        {hentTekst('forside.overgangsstønad.varselRegelendring2026.tredjeAvsnitt', intl)}
-      </BodyLong>
+      <BodyLong spacing>{hentTekst(førsteAvsnittNøkkel, intl)}</BodyLong>
+      <BodyLong spacing>{hentTekst(andreAvsnittNøkkel, intl)}</BodyLong>
+      <BodyLong spacing>{hentTekst(tredjeAvsnittNøkkel, intl)}</BodyLong>
     </Alert>
   );
 };
