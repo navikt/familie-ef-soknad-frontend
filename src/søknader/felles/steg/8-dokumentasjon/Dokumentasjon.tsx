c7851133cc1d2f4f991ed003379f87681536b5a9
zubaiersayed96 <zubaier.sayed@nav.no>
2026-09-14 13:58:13 +0200

    Nav-30710: Migrate from Sentry to @nais/apm for error tracking and observability (#2283)
    * Migrate from Sentry to @nais/apm for error tracking and observability
    
    * Configure @nais/apm initialization with tracing and environment settings
    
    * Add @nais registry to .npmrc and integrate error capturing in StegNavigasjon and Dokumentasjon components
    
    * Add @grafana/faro-web-tracing dependency for enhanced tracing capabilities
    
    * Add Nais meta tags injection to HTML decorator for enhanced telemetry
    
    * Simplify @nais/apm initialization by removing redundant configuration parameters
    
    * Add frontend configuration for nais.js and update telemetry URL
    
    * Update APM initialization with application name and namespace

--- src/søknader/felles/steg/8-dokumentasjon/Dokumentasjon.tsx
+++ src/søknader/felles/steg/8-dokumentasjon/Dokumentasjon.tsx
@@ -5,6 +5,7 @@ import LastOppVedlegg from '../../../felles/steg/8-dokumentasjon/LastOppVedlegg'
 import { NavigasjonState, Side } from '../../../../components/side/Side';
 import { IDokumentasjon } from '../../../../models/steg/dokumentasjon';
 import { erVedleggstidspunktGyldig } from '../../../../utils/dato';
+import { captureMessage } from '@nais/apm';
 import { useDebouncedCallback } from 'use-debounce';
 import { useLokalIntlContext } from '../../../../context/LokalIntlContext';
 import { hentHTMLTekst, hentTekst } from '../../../../utils/teksthåndtering';
@@ -46,6 +47,7 @@ const Dokumentasjon: React.FC = () => {
           erVedleggstidspunktGyldig(vedlegg.tidspunkt)
         );
         if (gyldigeVedlegg.length !== dokBehov.opplastedeVedlegg.length) {
+          captureMessage(`Fjernet ugyldig vedlegg fra søknaden.`, 'warning');
           oppdaterDokumentasjon(dokBehov.id, gyldigeVedlegg, dokBehov.harSendtInn);
         }
       }
