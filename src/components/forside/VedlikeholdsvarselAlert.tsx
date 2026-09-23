433e31a81474212372638910af8e88af23325134
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-09-23 10:44:57 +0200

    Senterer

--- src/components/forside/VedlikeholdsvarselAlert.tsx
+++ src/components/forside/VedlikeholdsvarselAlert.tsx
@@ -41,8 +41,8 @@ export const VedlikeholdsvarselAlert: React.FC = () => {
         position: 'relative',
         left: '50%',
         right: '50%',
-        marginLeft: '-50vw',
-        marginRight: '-50vw',
+        marginLeft: '-40vw',
+        marginRight: '-40vw',
       }}
     >
       <GlobalAlert.Header>
